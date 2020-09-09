8e161916fe4b848d46f282ea2a6a484e09eafc08
Shubham Upreti <shubham.upreti@gmail.com>
2020-09-10 01:56:30 +0530

    Lanes identified in an image

--- lane.py
+++ lane.py
@@ -18,6 +18,36 @@ import numpy as np
 """
 
 
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1 * 3 / 5)
+    x1 = (y1 - intercept) // slope
+    x2 = (y2 - intercept) // slope
+
+    return np.array([x1, y1, x2, y2])
+
+
+def average_slop_intercept(image, lines):
+    left_fit = []
+    right_lift = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, y1), (x2, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_lift.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(left_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+
+    return np.array(left_line, right_line)
+
+
 def canny(image):
     # Processing a single channel is less compute expensive
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -25,6 +55,15 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
 
     return canny
+
+
+def display_lines(image, lines):
+    line_img = np.zeros_like(image)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            cv2.line(line_img, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+    return line_img
 
 
 def region_of_interest(image):
@@ -37,11 +76,32 @@ def region_of_interest(image):
     return masked_img
 
 
+"""
+Parametric Space
+Hough space: (slope, y-intercept):(x, y)
+Instead of a line, single point is used
+Cartesian space: family of lines pass through a point
+Split hough space into a grid
+Point of intersection in a single bin, voting on grids
+Bin with most votes, gives point (m, b) which corresponds to line of best fit
+
+- Problem: Vertical lines with infinte slope can't be represented
+Instead of cartesian co-ordiantes, we can take polar co-ordinates
+r = x cos(n) + y sin(n) :n -> angle
+Line of best fit can be detected by intersection of curves
+"""
+
 img = cv2.imread("test_image.jpg")
 lane_img = np.copy(img)
-canny = canny(lane_img)
-cropped_img = region_of_interest(canny)
-cv2.imshow("Canny", cropped_img)
+canny_img = canny(lane_img)
+cropped_img = region_of_interest(canny_img)
+lines = cv2.HoughLinesP(
+    cropped_img, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5
+)
+averaged_lines = average_slop_intercept(lane_img, lines)
+line_img = display_lines(lane_img, averaged_lines)
+combo = cv2.addweighted(lane_img, 0.8, line_img, 1, 1)
+cv2.imshow("Result", combo)
 cv2.waitKey(0)
 
 # plt.imshow(canny)
